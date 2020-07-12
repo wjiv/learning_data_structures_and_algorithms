ad9e11f96a2c5c18f406c31c131354732740d600
wjiwang <48338570+wjiwang@users.noreply.github.com>
2020-07-12 10:33:15 +0800

    add stack and queue

---
+++ data-structures-LinearStructures-StackAndQueue.cpp
@@ -0,0 +1,33 @@
+//线性结构——栈与队列完整代码
+
+#include <iostream>
+using namespace std;
+
+typedef int ElementType;
+
+#pragma region 栈部分开始
+
+//栈结构
+struct Stack
+{
+    ElementType *data;
+    int top;
+};
+
+//栈测试函数
+void testStack()
+{
+}
+
+#pragma endregion 栈部分结束
+
+#pragma region main函数部分开始
+
+//main函数
+int main()
+{
+    testStack();
+    return 0;
+}
+
+#pragma endregion main函数部分结束
